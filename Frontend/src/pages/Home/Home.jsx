e4dc54f3cb759bb9b7c130490dc8c80c4956e851
sabrinarauj <sabrina65bina@gmail.com>
2025-09-30 21:26:12 -0300

    estruturando página Home

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,25 @@
 import './Home.css'
+import Fada from "../../assets/imagens/Fada.svg";
+import EstrelaRosa from "../../assets/imagens/EstrelaRosa.svg"
 function Home() {
     return (
         <>
-            <h1>Bem-vindo,</h1>
-            <h1>Pequeno leitor</h1>
-            
+            <main>
+                <section className='paginaPrincipal'>
+                    <hgroup className='titulos'>
+                        <h1>Bem-vindo,</h1>
+                        <h1>Pequeno leitor</h1>
+                    </hgroup>
+                    <button className='scroll'>
+                        ↓
+                    </button>
+                    <figure className='fada'>
+                        <img src={Fada} alt='imagem da mascote da fada' />
+                    </figure>
+
+                    <img src={EstrelaRosa} alt='estrela rosa' className='estrela estrela1' />
+                </section>
+            </main>
         </>
     )
 }
