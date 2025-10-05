1cfc08ed81bce5197e76598f01e2a9effe13484c
sabrinarauj <sabrina65bina@gmail.com>
2025-10-05 02:21:30 -0300

    novamente home em andamento

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -6,11 +6,11 @@ import PerfilFada3 from '../../assets/Images/PerfilFada3.svg'
 import EstrelaRosa from '../../assets/Images/EstrelaRosa.svg'
 import EstrelaBranca from '../../assets/Images/EstrelaBranca.svg'
 import NuvemAnimacao from '../../assets/Images/NuvemAnimacao.svg'
-import Nuvem from '../../assets/Images/Nuvem.svg'
 import BolinhaBranca from '../../assets/Images/BolinhaBranca.svg'
 import DecoracaoTexto from '../../assets/Images/DecoracaoTexto.svg'
 import DecoracaoTexto2 from '../../assets/Images/DecoracaoTexto2.svg'
 import DecoracaoTexto3 from '../../assets/Images/DecoracaoTexto3.svg'
+import Nuvem from '../../assets/Images/Nuvem.svg'
 import { ArrowDown } from 'phosphor-react'
 import Cards from '../../Components/CardsHome/Cards'
 
@@ -22,14 +22,10 @@ function Home() {
             <main>
                 <section className='paginaPrincipal'>
                     <div className='titulos'>
-                        <h1>Bem-vindo,<br />
+                        <h1>Bem-vindo,
                             Pequeno leitor
                         </h1>
                     </div>
-
-                    <button className='scroll'>
-                        <ArrowDown size={20} />
-                    </button>
 
                     <figure className='fada'>
                         <img src={FadaPrincipal} alt='imagem da mascote da fada' />
@@ -43,17 +39,34 @@ function Home() {
                     <img src={EstrelaBranca} alt='estrela branca' className='estrela5' />
                     <img src={EstrelaRosa} alt='estrela rosa' className='estrela6' />
                     <img src={BolinhaBranca} alt='bolinha branca de decoração' className='bolinha1' />
+                    <img src={BolinhaBranca} alt='bolinha branca de decoração' className='bolinha2' />
                     <img src={NuvemAnimacao} alt='nuvem branca animada' className='nuvem1' />
                     <img src={NuvemAnimacao} alt='nuvem branca animada' className='nuvem2' />
 
-                    <img src={Nuvem} alt='nuvem branca' className='nuvem3' />
+                    
+                    <button 
+                    className='scroll'
+                    onClick={() => document.getElementById('cards').scrollIntoView({ behavior: 'smooth'})}
+                    >
+                        <ArrowDown size={30} />
+                    </button>
+
                 </section>
 
-                <section className='secaoCards'>
-                    <Cards />
-                    <div className='nuvens'>
+                <section className='secaoCards' id='cards'>
+                      <div className='nuvens'>
                         <img src={Nuvem} alt='nuvem branca' id='nuvemSecao1' />
-                    </div>
+                        <img src={Nuvem} alt='nuvem branca' id='nuvemSecao2' />
+                    </div> 
+
+                    <Cards />
+
+                     <div className='nuvens'>
+                        <img src={Nuvem} alt='nuvem branca' id='nuvemSecao3' />
+                        <img src={Nuvem} alt='nuvem branca' id='nuvemSecao4' />
+                        <img src={Nuvem} alt='nuvem branca' id='nuvemSecao5' />
+                        <img src={Nuvem} alt='nuvem branca' id='nuvemSecao6' />
+                    </div> 
                 </section>
 
                 <section className='paginaFada'>
@@ -61,16 +74,16 @@ function Home() {
                     <img src={DecoracaoTexto} alt='decoração da caixa de texto' id='decoracaoTexto' />
 
                     <div class='caixaTexto'>
-                        <h2>Olá, eu sou a Bina!</h2>
-                        <p>Eu sou uma fada e sua melhor amiga!
+                        <h2>Olá,</h2>
+                        <h2>eu sou a Bina!</h2>
+                        <p>Eu sou uma fada e sua melhor amiga!</p>
                             Vou te acompanhar e auxiliar nessa
                             jornada de leitura e muito
                             aprendizado!
-                        </p>
                     </div>
 
-                <img src={PerfilFada2} alt='perfil da fada' id='fadaPerfil' />
-                <img src={DecoracaoTexto2} alt='decoração da segunda caixa de texto' id='decoracaoTexto' />
+                <img src={PerfilFada2} alt='perfil da fada' id='fadaPerfil2' />
+                <img src={DecoracaoTexto2} alt='decoração da segunda caixa de texto' id='decoracaoTexto2' />
                     <div class='caixaTexto'>
                         <h2>O que você pode fazer?</h2>
                         <p>Nesse site, você pode tanto jogar
@@ -79,12 +92,10 @@ function Home() {
                         </p>
                     </div>
 
-                <img src={PerfilFada3} alt='perfil da fada' id='fadaPerfil' />
+                <img src={PerfilFada3} alt='perfil da fada' id='fadaPerfil3' />
                 <img src={DecoracaoTexto3} alt='decoração da terceira caixa de texto' id='decoracaoTexto'/>
                     <div class='caixaTexto'>
                         <h2>Aproveite essa experiência</h2>
-                        <p>Biro biro biro
-                        </p>
                     </div>
                 </section>
 
