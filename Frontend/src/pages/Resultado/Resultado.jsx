02694373c8ce26bf75101decfbdfcdc49ccacb7d
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-10-07 08:58:32 -0300

    Add files via upload

---
+++ Frontend/src/pages/Resultado/Resultado.jsx
@@ -0,0 +1,40 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { RotateCcw, House } from "lucide-react";
+
+import estrelas from "../../Assets/Images/estrelas.png";
+import Nuvem from "../../Assets/Images/nuvens.svg";
+import fada from "../../Assets/Images/fadapontos.svg";
+
+import "./Resultado.css";
+
+export default function Resultado() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const pontos = location.state?.pontos || 0;
+
+  return (
+    <div className="resultado-container">
+      <img src={estrelas} alt="Estrelas" className="estrelas-bg" />
+
+      <div className="resultado">
+        <h1>Você concluiu o jogo!</h1>
+        <p>
+          Você acertou <strong>{pontos}</strong> de 15 palavras!
+        </p>
+
+        <div className="btns">
+          <button onClick={() => navigate("/")}>
+            <RotateCcw /> Jogar novamente
+          </button>
+          <button onClick={() => navigate("/home")}>
+            <House /> Página Inicial
+          </button>
+        </div>
+
+        <img src={fada} alt="Fada" className="fada" />
+      </div>
+
+      <img src={Nuvem} alt="Nuvens" className="nuvens-bg" />
+    </div>
+  );
+}
