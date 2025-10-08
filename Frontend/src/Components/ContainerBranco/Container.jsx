ea6371a22e359dc1805a82ac61bbb56a3f294f2d
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-10-08 06:06:12 -0300

    Revisão

--- Frontend/src/Components/ContainerBranco/Container.jsx
+++ Frontend/src/Components/ContainerBranco/Container.jsx
@@ -1,17 +1,15 @@
 import "./Container.css";
 
 /**
- * Componente Container
- * Responsável por agrupar e centralizar o conteúdo principal
- * dentro de um contêiner branco estilizado.
+ * Componente Container para agrupar e centralizar conteúdo
  *
- * @param {React.ReactNode} children - Conteúdo interno a ser renderizado dentro do contêiner.
- * @returns {JSX.Element} Estrutura de contêiner.
+ * @param {React.ReactNode} children - Conteúdo interno a ser renderizado dentro do contêiner
+ * @returns {JSX.Element} Estrutura do contêiner
  */
 
 function Container({ children }) {
   return (
-    <div className="white-container">
+    <div className="whiteContainer">
       {/* Renderiza o conteúdo passado como filho */}
       {children}
     </div>
