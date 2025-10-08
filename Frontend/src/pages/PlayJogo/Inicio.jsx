efffa2728d03dda9d44687e391f6568a70ca4730
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-10-08 15:58:22 -0300

    Update Inicio.jsx

--- Frontend/src/pages/PlayJogo/Inicio.jsx
+++ Frontend/src/pages/PlayJogo/Inicio.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Play } from "lucide-react";
 
 import ButtonTop from "../../Components/ButtonTop/ButtonTop.jsx";
 import ActionButton from "../../Components/ActionButtons/ActionButton.jsx";
 import Container from "../../Components/ContainerBranco/Container.jsx";
-import ModalSair from "../../Components/ModalSair/ModalSair.jsx";
 
 import Nuvens from "../../Assets/Images/Nuvens.svg";
 import Estrelas_fundo from "../../Assets/Images/Estrelas_fundo.png";
@@ -14,37 +12,24 @@ import Fada_rindo from "../../Assets/Images/Fada_rindo.png";
 
 import "./Inicio.css";
 
-/**
- * Página inicial do jogo "O Que Você Vê?"
- * Exibe o título do jogo, botões de navegação e modal de confirmação de saída.
+/** Página inicial do jogo
  *
- * @returns {JSX.Element} Página inicial do jogo.
+ * @returns {JSX.Element} Página
  */
 
 function Inicio() {
-  // Estado de controle do modal de saída
-  const [mostrarModal, setMostrarModal] = useState(false);
-
-  // Função que abre o modal
-  // const handleOpenModal = () => setMostrarModal(true);
-
-  // Função que fecha o modal
-  const handleCloseModal = () => setMostrarModal(false);
-
-  // Função que confirma a saída do usuário
-  const handleConfirmExit = () => {
-    setMostrarModal(false);
-  };
-
   return (
     <section className="container">
       {/* Fundo de estrelas */}
-      <img src={Estrelas_fundo} alt="estrelas" className="estrelas-bg" />
+      <img src={Estrelas_fundo} alt="estrelas" className="estrelasBg" />
 
       {/* Botão superior (home) */}
-      <ButtonTop >
-        <img src={iconCasa} alt="ícone home" />
-      </ButtonTop>
+      <Link to="/">
+        <ButtonTop >
+          <img src={iconCasa} alt="ícone home" />
+        </ButtonTop>
+      </Link>
+
 
       {/* Conteúdo principal do container */}
       <Container>
@@ -53,13 +38,16 @@ function Inicio() {
           <h1 className="subtitle">O Que Você Vê?</h1>
 
           {/* Botões principais */}
-          <section className="buttons-container">
-            <ActionButton
-              icon={<Play size={28} color="white" />}
-              onClick={() => console.log("Jogar clicado")}
-            >
-              JOGAR
-            </ActionButton>
+          <section className="buttonsContainer">
+            <Link to="/jogo">
+              <ActionButton
+                icon={<Play size={28} color="white" />}
+                onClick={() => console.log("Jogar clicado")}
+              >
+                JOGAR
+              </ActionButton>
+            </Link>
+
 
             <Link to="/comojogar1">
               <ActionButton onClick={() => console.log("Como jogar clicado")}>
@@ -72,14 +60,7 @@ function Inicio() {
 
       {/* Imagens decorativas */}
       <img src={Fada_rindo} alt="fada sorrindo" className="fadaSorrindo" />
-      <img src={Nuvens} alt="nuvens" className="nuvens-bg" />
-
-      {/* Modal de confirmação de saída */}
-      <ModalSair
-        isOpen={mostrarModal}
-        onClose={handleCloseModal}
-        onConfirm={handleConfirmExit}
-      />
+      <img src={Nuvens} alt="nuvens" className="nuvensBg" />
     </section>
   );
 }
